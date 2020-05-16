bbd349427e55235c0973b66ca6ec042ede4a8caa
Alex Williams <hughaw64@gmail.com>
2020-05-16 21:18:50 +0100

    Added blocking algorithm

--- src/genetic_ai.rs
+++ src/genetic_ai.rs
@@ -1,14 +1,14 @@
 use crate::lib;
 use rand::prelude::*;
 
+const GENE_COUNT: usize = 3;
 lazy_static! {
     static ref GENES: [std::sync::Arc<dyn ActionScorer>; GENE_COUNT] = [
         std::sync::Arc::new(PrioritizeClimbing {}),
         std::sync::Arc::new(PrioritizeCapping {}),
+        std::sync::Arc::new(PrioritizeBlocking {}),
     ];
 }
-
-const GENE_COUNT: usize = 2;
 
 trait ActionScorer: Sync + Send {
     fn get_score(
@@ -52,16 +52,66 @@ impl ActionScorer for PrioritizeCapping {
     }
 }
 
-#[derive(Copy, Clone, Debug)]
+struct PrioritizeBlocking {}
+impl ActionScorer for PrioritizeBlocking {
+    fn get_score(
+        &self,
+        game: &lib::Game,
+        player_id: usize,
+        worker: lib::Worker,
+        movement: (u8, u8),
+        b: (u8, u8),
+    ) -> i32 {
+        let mut nearby_player = false;
+        for (player, (w1, w2)) in game.player_locations.iter().enumerate() {
+            if player != player_id {
+                for &w in &[w1, w2] {
+                    if (w.0 as i8 - b.0 as i8).abs() <= 1 && (w.1 as i8 - b.1 as i8).abs() <= 1 {
+                        nearby_player = true;
+                    }
+                }
+            }
+        }
+        if !nearby_player {
+            0
+        } else {
+            let mut max_near_height = 0;
+            for &s in &[
+                (b.0 - 1, b.1 - 1),
+                (b.0, b.1 - 1),
+                (b.0 + 1, b.1 - 1),
+                (b.0 - 1, b.1),
+                (b.0 - 1, b.1 + 1),
+                (b.0, b.1 + 1),
+                (b.0 + 1, b.1),
+                (b.0 + 1, b.1 + 1),
+            ] {
+                if s.0 <= 4 && s.1 <= 4 {
+                    max_near_height =
+                        max_near_height.max(game.board[s.0 as usize][s.1 as usize].to_int());
+                }
+            }
+
+            let current_height = game.board[b.0 as usize][b.1 as usize].to_int();
+            match current_height.cmp(&max_near_height) {
+                std::cmp::Ordering::Less => 0,
+                std::cmp::Ordering::Equal => 1,
+                std::cmp::Ordering::Greater => 2,
+            }
+        }
+    }
+}
+
+#[derive(PartialEq, Copy, Clone, Debug)]
 pub struct GeneticAI {
-    gene_weighting: [u16; GENE_COUNT],
+    pub gene_weighting: [u16; GENE_COUNT],
     //rng: rand::rngs::thread::ThreadRng
 }
 
 impl GeneticAI {
     pub fn new() -> Self {
         Self {
-            gene_weighting: [10, 10],
+            gene_weighting: [0, 0, 0],
         }
     }
 }
