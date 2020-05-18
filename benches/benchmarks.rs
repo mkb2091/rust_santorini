550fc258ae8e1a8a11f8f78f11f4c26249826b83
Alex Williams <hughaw64@gmail.com>
2020-05-18 10:40:04 +0100

    Added new benchmark

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -10,6 +10,14 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("list_possible_actions_on_empty", |b| {
         b.iter(|| game.list_possible_actions(0))
     });
+    let game = Game {
+        board: [[TowerStates::Empty; 5]; 5],
+        player_locations: [((0, 0), (4, 4)), ((17, 17), (17, 17)), ((17, 17), (17, 17))],
+        player_statuses: [Status::Playing, Status::Dead, Status::Dead],
+    };
+    c.bench_function("list_possible_actions_on_empty_near_edge", |b| {
+        b.iter(|| game.list_possible_actions(0))
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
