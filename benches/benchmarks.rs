967e38b092d0db6fd1031135feed6ae1be489875
Alex Williams <hughaw64@gmail.com>
2020-05-19 09:25:23 +0100

    Added new benchmark

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,6 +1,8 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use rust_santorini::{Game, Status, TowerStates};
 
+const TSE: TowerStates = TowerStates::Empty;
+const TS2: TowerStates = TowerStates::Level2;
 fn criterion_benchmark(c: &mut Criterion) {
     let game = Game {
         board: [[TowerStates::Empty; 5]; 5],
@@ -16,6 +18,21 @@ fn criterion_benchmark(c: &mut Criterion) {
         player_statuses: [Status::Playing, Status::Dead, Status::Dead],
     };
     c.bench_function("list_possible_actions_on_empty_near_edge", |b| {
+        b.iter(|| game.list_possible_actions(0))
+    });
+
+    let game = Game {
+        board: [
+            [TS2; 5],
+            [TS2; 5],
+            [TS2, TS2, TSE, TSE, TS2],
+            [TS2; 5],
+            [TS2; 5],
+        ],
+        player_locations: [((2, 2), (2, 3)), ((17, 17), (17, 17)), ((17, 17), (17, 17))],
+        player_statuses: [Status::Playing, Status::Dead, Status::Dead],
+    };
+    c.bench_function("list_possible_actions_where_trapped", |b| {
         b.iter(|| game.list_possible_actions(0))
     });
 }
